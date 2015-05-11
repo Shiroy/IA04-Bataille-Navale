6271df0fb3e54425eb440f3aff428a9f8a3391c4
Travis <travis@travis-ci.org>
2015-05-11 17:34:11 +0200

    Fichier main

---
+++ src/launch/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package launch;
+
+/**
+ * @author antoine
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
