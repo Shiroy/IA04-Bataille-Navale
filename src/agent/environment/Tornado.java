a048b91036102dc5f7e94c3206fdfe0fab179aac
DavidLucchesi <lucchesi.elypse@gmail.com>
2015-05-18 19:13:41 +0200

    Tornado v2

--- src/agent/environment/Tornado.java
+++ src/agent/environment/Tornado.java
@@ -1,7 +1,9 @@
 package agent.environment;
 
+import application.state.BattleShip;
 import sim.engine.SimState;
 import sim.engine.Steppable;
+import sim.util.Int2D;
 
 public class Tornado implements Steppable{
 
@@ -13,6 +15,15 @@ public class Tornado implements Steppable{
 	@Override
 	public void step(SimState state) {
 		
+	}
+	
+	public void action(SimState state){
+		BattleShip bs = (BattleShip)state;
+        Int2D location = bs.map.getObjectLocation(this);
+        @SuppressWarnings("unused")
+		int x = location.x;
+        @SuppressWarnings("unused")
+        int y = location.y;
 	}
 
 }
