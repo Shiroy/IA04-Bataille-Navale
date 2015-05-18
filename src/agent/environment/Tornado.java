ecb25ad6898e9be16d9dd758551426bb1043a05e
DavidLucchesi <lucchesi.elypse@gmail.com>
2015-05-18 19:05:17 +0200

    Tornado

---
+++ src/agent/environment/Tornado.java
@@ -0,0 +1,18 @@
+package agent.environment;
+
+import sim.engine.SimState;
+import sim.engine.Steppable;
+
+public class Tornado implements Steppable{
+
+	private static final long serialVersionUID = 1L;
+
+	public Tornado() {
+	}
+
+	@Override
+	public void step(SimState state) {
+		
+	}
+
+}
