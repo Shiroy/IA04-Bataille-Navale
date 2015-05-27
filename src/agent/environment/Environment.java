d0c7b5df5317e9817c14539aab476d7cb65ec9a3
DavidLucchesi <lucchesi.elypse@gmail.com>
2015-05-27 17:20:18 +0200

    Modify Environment with mapPortrayal and Singleton

--- src/agent/environment/Environment.java
+++ src/agent/environment/Environment.java
@@ -1,12 +1,28 @@
 package agent.environment;
 
-import application.state.BattleShip;
+import sim.display.GUIState;
 import sim.engine.SimState;
 import sim.engine.Steppable;
+import sim.portrayal.grid.SparseGridPortrayal2D;
+import application.state.BattleShip;
+import application.state.BattleShipWithUI;
 
 @SuppressWarnings("serial")
 public class Environment implements Steppable {
 	private EnvironmentStrategy behaviourStrategy;
+	private static Environment m_instance = null;
+	private SparseGridPortrayal2D mapPortrayal;
+	
+	public static Environment getInstance() {
+		if(m_instance == null)
+			m_instance = new Environment();
+		return m_instance;
+	}
+	
+	public void setMap(GUIState gui){
+		BattleShipWithUI bswu = (BattleShipWithUI) gui;
+		//this.mapPortrayal=bswu.getMap;
+	}
 
 	public Environment() {
 		
@@ -14,14 +30,18 @@ public class Environment implements Steppable {
 
 	@Override
 	public void step(SimState state) {
-		@SuppressWarnings("unused")
 		BattleShip bs = (BattleShip) state;
-		behaviourStrategy.action(this, state);
+		behaviourStrategy.action(this, bs);
 	}
 	
 	public void setStrategy(EnvironmentStrategy behaviour){
 		behaviourStrategy = behaviour;
 		behaviourStrategy.init(this);
+	}
+	
+	public void createTornado(){
+		Tornado tornado = new Tornado();
+		mapPortrayal.setObjectPosition(tornado, null, null);
 	}
 	
 
