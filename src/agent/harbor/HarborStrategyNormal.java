439b1432ce14b9404aec9f130aca4ef0416b7ccc
tomwang1215 <tomwang1215@gmail.com>
2015-05-27 21:54:15 +0200

    Change the name and add more detail

---
+++ src/agent/harbor/HarborStrategyNormal.java
@@ -0,0 +1,43 @@
+package agent.harbor;
+
+import sim.engine.SimState;
+import sim.util.Int2D;
+import agent.ship.Ship;
+import agent.ship.ShipFactory;
+import agent.ship.ShipTemplate;
+import agent.ship.ShipMessage.ShootReceived;
+import application.state.BattleShip;
+
+public class HarborStrategyNormal implements HarborStrategy {
+
+	/**
+	 * The strategy for harbor when it is safe
+	 */
+	
+	@Override
+	public void init(Harbor harbor, SimState state) {
+		BattleShip bs = (BattleShip)state;
+		harbor.setPosition(bs.map.getObjectLocation(this));
+	}
+
+	@Override
+	public void action(Harbor harbor, SimState state) {
+		//TODO add strategy for different type of ship
+		String shipName = null;// TODO Change the name
+		Ship newShip = harbor.createShip(shipName);
+		BattleShip bs = (BattleShip)state;
+		Int2D position = harbor.getPosition();
+		int x = position.x + 1; 
+		int y = position.y + 1;
+		bs.map.setObjectLocation(newShip, x, y);
+		bs.schedule.scheduleRepeating(newShip);
+		
+		//TODO Handle the messages. add alart message
+	}
+
+	@Override
+	public void attacked(Harbor harbor, ShootReceived shoot) {
+		
+	}
+
+}
