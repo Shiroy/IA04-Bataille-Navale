55ea72f089eef1edbf55741453f2f821ac554359
cmascart <mascart.c@laposte.net>
2015-05-27 17:36:43 +0200

    Ajout de la strategie aleatoire

--- src/agent/ship/ShipStrategyHazardous.java
+++ src/agent/ship/ShipStrategyHazardous.java
@@ -1,37 +1,76 @@
 package agent.ship;
 
 import sim.engine.SimState;
+import sim.field.grid.Grid2D;
+import sim.util.Bag;
 import sim.util.Int2D;
+import sim.util.IntBag;
+import agent.ship.ShipMessage.EnnemyKilled;
+import agent.ship.ShipMessage.EnvironmentDamage;
+import agent.ship.ShipMessage.ShootReceived;
 import application.state.BattleShip;
 
 public class ShipStrategyHazardous implements ShipStrategy {
 
 	@Override
 	public void init(Ship ship) {
-		
+
 	}
 
 	@Override
 	public void action(Ship ship, SimState state) {
-		BattleShip bs = (BattleShip)state;
-        
-        Int2D location = bs.map.getObjectLocation(this);
-        int x = location.x;
-        int y = location.y;
-        
-        
-	}
+		BattleShip bs = (BattleShip) state;
 
-	@Override
-	public void shootReceived(Ship ship) {
-		// TODO Auto-generated method stub
+		Int2D location = bs.map.getObjectLocation(this);
+		int range = ship.getTemplate().getShootRange();
+		Bag result = new Bag();
+		IntBag xPos = new IntBag();
+		IntBag yPos = new IntBag();
+		bs.map.getRadialNeighborsAndLocations(location.x, location.y, range,
+				Grid2D.TOROIDAL, false, Grid2D.ALL, false, result, xPos, yPos);
+		
+		boolean action = false;
+		
+		if (result.size() != 0) {
+			for (Object obj : result)
+				if (obj instanceof Ship) {
+					Ship s = (Ship) obj;
+					if (s.getFaction() != ship.getFaction()) {
+						// TODO
+						; // Attack the ship
+						action = true; // If attack succeed
+					}
+				}
+		} else {
+			range = ship.getTemplate().getViewRange();
+			result = new Bag();
+			xPos = new IntBag();
+			yPos = new IntBag();
+			bs.map.getRadialNeighborsAndLocations(location.x, location.y, range,
+					Grid2D.TOROIDAL, false, Grid2D.ALL, false, result, xPos, yPos);
+			for (Object obj : result)
+				if (obj instanceof Ship && !action) {
+					Ship s = (Ship) obj;
+					if (s.getFaction() != ship.getFaction()) {
+						// TODO
+						; // Move to the ship
+						action = true;
+					}
+				}
+		}
 		
 	}
 
 	@Override
-	public void ennemyKilled(Ship killer, Ship killed) {
-		// TODO Auto-generated method stub
-		
+	public void shootReceived(Ship ship, ShootReceived damage) {
+		//TODO
 	}
 
+	@Override
+	public void ennemyKilled(Ship killer, EnnemyKilled killInfo) {
+	}
+
+	@Override
+	public void environmentDamage(Ship victim, EnvironmentDamage damage) {
+	}
 }
