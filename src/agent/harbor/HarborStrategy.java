d51acc1bb96d54e98d7a6bd1f0dc67324db2291d
tomwang1215 <tomwang1215@gmail.com>
2015-05-27 15:48:28 +0200

    Add commits and 2 methodes

--- src/agent/harbor/HarborStrategy.java
+++ src/agent/harbor/HarborStrategy.java
@@ -1,9 +1,37 @@
 package agent.harbor;
 
+import agent.ship.Ship;
+import sim.engine.SimState;
+
 public interface HarborStrategy {
 	
+	/**
+	 * Called when a Strategy first bound to a Harbor
+	 * 
+	 * @param harbor The harbor the Strategy was bounded to
+	 */
 	public void init(Harbor harbor);
 	
+	/**
+	 * Called every simulation cycle to update the strategy 
+	 * 
+	 * @param harbor The harbor the strategy was bounded to
+	 */
 	public void action(Harbor harbor);
+	
+	/**
+	 * Called when the harbor is under attack
+	 * 
+	 * @param harbor The harbor that is under attack
+	 */
+	public void attacked(Harbor harbor);
+	
+	/**
+	 * Called when the harbor is occupied by an other faction
+	 * 
+	 * @param harbor	The harbor that is occupied
+	 * @param occupant	The faction that occupy the harbor
+	 */
+	public void occupied(Harbor harbor, Faction occupant);
 	
 }
